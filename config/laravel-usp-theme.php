8d39c3ae1f57b29398636463673f86496b5f2ed8
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-02-01 17:21:16 -0300

    teste

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -1,47 +1,70 @@
 <?php
 
-$submenu1 =  [
+$submenu1 = [
     [
-        'text' => 'SubItem 1',
-        'url'  => '/subitem1'
+        'text' => '<i class="fas fa-atom"></i>  SubItem 1',
+        'url' => config('app.url') . '/subitem1',
     ],
     [
         'text' => 'SubItem 2',
-        'url'  => '/subitem2',
-        'can'  => 'admin',
+        'url' => config('app.url') . '/subitem2',
+        'can' => 'admin',
     ],
     [
         'text' => 'SubItem 3',
-        'url'  => '/subitem3',
+        'url' => config('app.url') . '/subitem3',
     ],
 ];
 
-$submenu2 =  [
+$submenu2 = [
     [
         'text' => 'SubItem 1',
-        'url'  => '/subitem1'
+        'url' => config('app.url') . '/subitem1',
     ],
     [
         'text' => 'SubItem 2',
-        'url'  => '/subitem2',
-        'can'  => 'admin',
+        'url' => config('app.url') . '/subitem2',
+        'can' => 'admin',
     ],
 ];
 
 return [
-    'title'=> 'USPdev',
-    'dashboard_url' => '/',
-    'logout_method' => 'GET',
-    'logout_url' => '/logout',
-    'login_url' => '/login',
+    'title' => config('app.name'),
+    'dashboard_url' => config('app.url'),
+    'logout_method' => 'POST',
+    'logout_url' => config('app.url') . '/logout',
+    'login_url' => config('app.url') . '/login',
     'menu' => [
         [
-            'text' => 'Ocorrências',
-            'url'  => '/ocorrencias'
+            'text' => '<i class="fas fa-home"></i> Item 1',
+            'url' => config('app.url') . '/item1',
         ],
         [
-            'text' => 'Usuários',
-            'url'  => '/users',
-        ]
-    ]
+            'text' => 'Item 2',
+            'url' => config('app.url') . '/item2',
+            'can' => '',
+        ],
+        [
+            'text' => 'Item 3',
+            'url' => config('app.url') . '/item3',
+            'can' => 'admin',
+        ],
+        [
+            'text' => 'SubMenu1',
+            'submenu' => $submenu1,
+        ],
+        [
+            'text' => 'SubMenu2',
+            'submenu' => $submenu2,
+            'can' => 'admin',
+        ],
+    ],
+    'right_menu' => [
+        [
+            'text' => '<i class="fas fa-cog"></i>',
+            'title' => 'Configurações',
+            'target' => '_blank',
+            'url' => config('app.url') . '/item1',
+        ],
+    ],
 ];
